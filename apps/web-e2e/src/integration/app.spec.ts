eeba7f886acc02fc70dde95e3e1ca4942f62d05a
leivaa <adrianleiva212@gmail.com>
2021-11-29 18:03:51 +0100

    chore: rename frontend to web

--- apps/web-e2e/src/integration/app.spec.ts
+++ apps/web-e2e/src/integration/app.spec.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('net-match', () => {
+describe('web', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('net-match', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to net-match!');
+    getGreeting().contains('Welcome to web!');
   });
 });
