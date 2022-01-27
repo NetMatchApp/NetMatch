4f6fcdcd9b4321c00671104d1b4cdf77b3875f5b
jdes01 <jasampa3000@gmail.com>
2022-01-27 14:57:26 +0100

    fix: change spelling createUserDTO -> createUserCommand

--- apps/api/src/user/application/useCases/createUser.service.ts
+++ apps/api/src/user/application/useCases/createUser.service.ts
@@ -2,9 +2,9 @@ import { User } from "../../domain/model/user";
 import { UserRepositoryInterface } from "../../domain/repository/userRepositoryInterface";
 import { CreateUserCommand } from "../commands/createUser.command"
 
-export function createUserService(userRepository: UserRepositoryInterface, createUserDTO: CreateUserCommand) {
+export function createUserService(userRepository: UserRepositoryInterface, createUserCommand: CreateUserCommand) {
 
-    const user = User.create(createUserDTO.name, createUserDTO.company, createUserDTO.position);
+    const user = User.create(createUserCommand.name, createUserCommand.company, createUserCommand.position);
     
     userRepository.saveUser(user);
 }
