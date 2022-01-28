85f1a6d071ccd9a88b9c9f1c6f3ff6402c76c58f
jdes01 <jasampa3000@gmail.com>
2022-01-28 02:30:31 +0100

    chore: installed cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,14 @@
 import * as express from 'express';
 import bodyParser = require('body-parser');
 import { userRouter } from './routes/user.routes'
+import * as cors from 'cors'
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(cors())
 
 
 app.use('/api/user', userRouter);
